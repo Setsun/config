ca2f16f94493b44ba0250077f5aa8013dfb488e8
Setsun <setsun@setsun.io>
2018-09-02 14:21:49 -0400

    fix(babel): remove stage-2 preset / replace with explicit plugins

--- packages/babel-preset-setsun/index.js
+++ packages/babel-preset-setsun/index.js
@@ -6,16 +6,26 @@ module.exports = function () {
         {
           targets: {
             browsers: [
-              "last 2 versions",
-              "safari >= 9",
-              "ie >= 11"
+              'last 2 versions',
+              'safari >= 9',
+              'ie >= 11'
             ]
           },
         },
       ],
-      require.resolve('@babel/preset-stage-2'),
       require.resolve('@babel/preset-react'),
       require.resolve('@babel/preset-typescript')
     ]
+    plugins: [
+      require.resolve('@babel/plugin-proposal-class-properties'),
+      require.resolve('@babel/plugin-proposal-decorators'),
+      require.resolve('@babel/plugin-proposal-export-namespace-from'),
+      require.resolve('@babel/plugin-proposal-function-sent'),
+      require.resolve('@babel/plugin-proposal-json-strings'),
+      require.resolve('@babel/plugin-proposal-numeric-separator'),
+      require.resolve('@babel/plugin-proposal-throw-expressions'),
+      require.resolve('@babel/plugin-syntax-dynamic-import'),
+      require.resolve('@babel/plugin-syntax-import-meta'),
+    ],
   };
 }
